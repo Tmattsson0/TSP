efd39700b6631bb3ca89794ba1045cf0c47d0bf8
Thomas Mattsson <tmattsson0@gmail.com>
2019-10-06 17:56:05 +0900

    Started work on genome

---
+++ src/com/company/Genome.java
@@ -0,0 +1,76 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Genome implements Comparable {
+    Singleton st = Singleton.getInstance();
+
+    List<City> genome;
+    double[][] travelCost;
+    City startingCity = st.cities.get(0);
+    int numberOfCities;
+    double fitness;
+
+    // Generates a random salesman
+    public Genome(int numberOfCities, City startingCity) {
+        this.startingCity = startingCity;
+        this.numberOfCities = numberOfCities;
+
+        this.genome = randomSalesman();
+        this.fitness = this.calculateFitness();
+    }
+
+    // Generates a salesman with a user-defined genome
+    public Genome(List<City> permutationOfCities, int numberOfCities, City startingCity) {
+        this.genome = permutationOfCities;
+        this.startingCity = startingCity;
+        this.numberOfCities = numberOfCities;
+
+        this.fitness = this.calculateFitness();
+    }
+
+    // Generates a random genome
+    // Genomes are permutations of the list of cities, except the starting city
+    // so we add them all to a list and shuffle
+    private List<City> randomSalesman() {
+        List<City> result = new ArrayList<City>();
+        for (int i = 0; i < numberOfCities; i++) {
+            if (i != startingCity)
+                result.add(i);
+        }
+        Collections.shuffle(result);
+        return result;
+    }
+
+
+
+
+
+    public double calculateFitness() {
+        double fitness = 0;
+        City currentCity = startingCity;
+
+        // Calculating path cost
+        for (City gene : genome) {
+            fitness += currentCity.distance(gene);
+            currentCity = gene;
+        }
+
+        // We have to add going back to the starting city to complete the circle
+        // the genome is missing the starting city, and indexing starts at 0, which is why we subtract 2
+        fitness += currentCity.distance(startingCity);
+
+        return fitness;
+    }
+
+
+
+
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,28 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
     Singleton st = Singleton.getInstance();
     FileRead fr = new FileRead();
     fr.readFile("/Users/thomasmattsson/Documents/GitHub/TSP/src/com/company/burma14.tsp");
+
+    //Calculate travel costs to and from all cities
+    double[][] travelCosts = new double[st.cities.size()][st.cities.size()];
+        for(int i = 0; i<st.cities.size(); i++){
+            for(int j=0; j<=i; j++){
+                if(i==j) {
+                    travelCosts[i][j] = 0.0;
+                } else {
+                    travelCosts[i][j] = st.cities.get(i).distance(st.cities.get(j));
+                    travelCosts[j][i] = travelCosts[i][j];
+                }
+            }
+        }
+
+        System.out.println(Arrays.deepToString(travelCosts));
 
 //    for (City c : st.cities) {
 //        System.out.println(c.toString());
